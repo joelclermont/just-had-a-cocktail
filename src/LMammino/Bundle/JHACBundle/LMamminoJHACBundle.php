31f2f959e52f04f81a609608b3757a2fe4d94632
Joel Clermont <jclermont@gmail.com>
2013-02-26 21:34:13 -0600

    removed unused images

--- src/LMammino/Bundle/JHACBundle/LMamminoJHACBundle.php
+++ src/LMammino/Bundle/JHACBundle/LMamminoJHACBundle.php
@@ -6,10 +6,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LMamminoJHACBundle extends Bundle
 {
-    public static function getImageDir()
-    {
-        return '/bundles/lmamminojhac/images/';
-    }
 
     public function getParent()
     {
